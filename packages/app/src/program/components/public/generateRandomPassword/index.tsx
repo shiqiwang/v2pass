82581953a7fc3827ef85f4329757d7cede8a44ef
emiwang <2585026122@qq.com>
2019-08-01 01:48:28 +0800

    产生随机密码组件，ui

---
+++ packages/app/src/program/components/public/generateRandomPassword/index.tsx
@@ -0,0 +1,35 @@
+import {Card, Icon, InputNumber, Rate, Switch} from 'antd';
+import {observable} from 'mobx';
+import React, {Component, ReactNode} from 'react';
+
+import './index.less';
+
+class GenerateRandomPassword extends Component {
+  @observable passwordComplexity = 0;
+
+  render(): ReactNode {
+    return (
+      <div className="generateRandomPassword">
+        <Card style={{width: 300}}>
+          <Icon className="renew" type="sync" />
+          <div className="row">
+            <span className="label">密码强度</span>
+            <Rate disabled defaultValue={this.passwordComplexity} />
+          </div>
+          <div className="row rowSecond">
+            <span className="label">密码长度</span>
+            <InputNumber min={6} max={36} />
+          </div>
+          <div className="row">
+            <span className="label">使用数字</span>
+            <Switch defaultChecked />
+            <span className="label">使用符号</span>
+            <Switch defaultChecked />
+          </div>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default GenerateRandomPassword;
