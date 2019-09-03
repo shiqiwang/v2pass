18ae95aaccb32a58acf660c9fc4e800526bdb540
emiwang <2585026122@qq.com>
2019-09-03 11:17:06 +0800

    修改util

--- packages/app/src/util/util.ts
+++ packages/app/src/util/util.ts
@@ -2,45 +2,42 @@ import Vault from '../types/vault';
 import Folder from '../types/folder';
 import Password from '../types/password';
 import {ActiveItem} from '../program/components/passwordList/passwordList';
-export function findVault(vaults: Vault[], _id: Vault['_id']): Vault {
-  return vaults.map(vault => {
-    if (vault._id === _id) {
-      return vault;
-    }
-  })[0];
+
+export function findVault(
+  vaults: Vault[],
+  _id: Vault['_id'],
+): Vault | undefined {
+  return vaults.filter(vault => vault._id === _id)[0];
 }
+
 export function findFolder(
   vaults: Vault[],
   _id: Folder['_id'],
   vaultId: Folder['vaultId'],
-): Folder {
-  return vaults.map(vault => {
-    if (vault._id === vaultId) {
-      return vault.folders.map(folder => {
-        if (folder._id === _id) {
-          return folder;
-        }
-      })[0];
-    }
-  })[0];
+): Folder | undefined {
+  const targetVault = vaults.filter(vault => vault._id === vaultId)[0];
+  if (targetVault) {
+    return targetVault.folders.filter(folder => folder._id === _id)[0];
+  }
+  return undefined;
 }
 
 export function findPassword(
   vaults: Vault[],
   activeItem: ActiveItem,
-): Password {
+): Password | undefined {
   const {activeFolder, activePassword, activeVault} = activeItem;
-  return vaults.map(vault => {
-    if (vault._id === activeVault) {
-      return vault.folders.map(folder => {
-        if (folder._id === activeFolder) {
-          return folder.passwords.map(password => {
-            if (password._id === activePassword) {
-              return password;
-            }
-          })[0];
-        }
-      })[0];
-    }
-  })[0];
+  const targetVault = vaults.filter(vault => vault._id === activeVault)[0];
+  if (!targetVault) {
+    return undefined;
+  }
+  const targetFolder = targetVault.folders.filter(
+    folder => folder._id === activeFolder,
+  )[0];
+  if (!targetFolder) {
+    return undefined;
+  }
+  return targetFolder.passwords.filter(
+    password => password._id === activePassword,
+  )[0];
 }
