4aa47bf0e8205330ad3834ee04792c4c4f5369fb
emiwang <2585026122@qq.com>
2019-09-26 15:21:17 +0800

    更改后端请求方法，对数据库的增删查改

--- packages/server/src/program/requestMethod.ts
+++ packages/server/src/program/requestMethod.ts
@@ -1,53 +1,139 @@
-import {Binary} from 'crypto';
+// 这个verify（验证器）只能被node查到，但是不能传递到前端
+// node和数据库会跑在同一个局域网中，需要保证服务器安全，及时升级系统，开启防火墙等
+// 所有用户输入的数据在路由（express）阶段都需要小心处理，看看是否带有恶意代码
+// 如果用户是登录的，可以用session验证身份，登录后生成的零时令牌
+// 后端的操作应尽量简单，一个操作或者api只做单一的一件事，以免出现漏洞被利用
+import mongodb from 'mongodb';
 
-// 注册时检测重名
-function checkUserName(username: string): boolean {
-  return true;
-}
+import {UnlockKey, User, UserWithVerify} from './types';
 
-function register(username: string, email: string, verify: string): boolean {
-  return true;
-}
+const MongoClient = mongodb.MongoClient;
+const url = 'mongodb://localhost:27017';
+const dbName = 'v2pass';
+const client = new MongoClient(url);
+const db = client.db(dbName);
+const collection = db.collection('test');
+
+// 使用索引保证插入数据的username和email的唯一性
+collection
+  .createIndex({username: 1, email: 1}, {unique: true})
+  .catch(console.error);
 
 function authenticate(
-  username: string,
-  email: string,
-  unlockKey: string,
-): object | false {
-  // 用户身份验证成功需要返回用户所有数据
-  const authenticateResult = true;
-
-  if (authenticateResult) {
-    return {};
+  unlockKey: UnlockKey,
+  verify: UserWithVerify['verify'],
+): boolean {
+  if (verify === unlockKey) {
+    // 这里应该用verify验证器来验证unlockKey才对!!!
+    return true;
   }
 
   return false;
 }
 
-// 加密数据应当是二进制类型的吗
-// updateData需要验证身份吗
-function updateData(
-  username: string,
-  email: string,
-  encryptData: Binary,
-  unlockKey?: string,
-): boolean {
-  return true;
+async function getUserById(_id: User['_id']): Promise<any> {
+  const result = await collection.find({_id}).toArray();
+
+  if (!result.length) {
+    throw Error('not found');
+  }
+
+  return result[0];
 }
 
-// 数据只有在用户身份验证通过后才能给
-// 这里返回值应当是用户全部的数据，暂时不知道类型
-function getData(username: string, email: string): boolean {
-  return true;
+async function getUserByUsernameEmail(
+  username: User['username'],
+  email: User['email'],
+): Promise<any> {
+  const result = await collection.find({username, email}).toArray();
+
+  if (!result.length) {
+    throw Error('not found');
+  }
+
+  return result[0];
 }
 
-// 账户设置
-function editAccount(
-  username: string,
-  email: string,
-  unlockKey: string,
-  newDataObject: object,
-  verify: string,
-): boolean {
-  return true;
+export async function testUserNameAvailability(
+  username: User['username'],
+): Promise<boolean> {
+  const num = await collection.count({username});
+  return !num;
+}
+
+export async function testEmailAvailability(
+  email: User['email'],
+): Promise<boolean> {
+  const num = await collection.count({email});
+  return !num;
+}
+
+// 注册时要有username email
+// verify指的是密码验证器，不能直接把密码传过来保存
+export async function register(
+  username: User['username'],
+  email: User['email'],
+  verify: UserWithVerify['verify'],
+): Promise<mongodb.InsertOneWriteOpResult> {
+  return collection.insertOne({
+    username,
+    email,
+    verify,
+    _id: mongodb.ObjectId,
+  });
+}
+
+export async function updateData(
+  _id: User['_id'],
+  data: User['data'],
+  unlockKey: UnlockKey,
+): Promise<mongodb.UpdateWriteOpResult> {
+  const user = await getUserById(_id);
+  const {verify} = user;
+  const result = await authenticate(unlockKey, verify);
+
+  if (result) {
+    return collection.updateOne({_id}, {$set: {data}});
+  }
+
+  throw Error('authentication failed');
+}
+
+// verify为新验证器，unlockKey需要传递过来用旧验证器验证一边先
+// 更改账户信息时，前端也需要查重用户名邮箱，且会导致unlockKey和verify的变化
+export async function updateAccount(
+  _id: User['_id'],
+  newUsername: User['username'],
+  newEmail: User['email'],
+  newVerify: UserWithVerify['verify'],
+  unlockKey: UnlockKey,
+): Promise<mongodb.UpdateWriteOpResult> {
+  const user = await getUserById(_id);
+  const {verify} = user;
+  const result = await authenticate(unlockKey, verify);
+
+  if (result) {
+    return collection.updateOne(
+      {_id},
+      {$set: {username: newUsername, email: newEmail, verify: newVerify}},
+    );
+  }
+
+  throw Error('authentication failed');
+}
+
+export async function getData(
+  username: User['username'],
+  email: User['email'],
+  unlockKey: UnlockKey,
+): Promise<any> {
+  const user = await getUserByUsernameEmail(username, email);
+  const {verify} = user;
+  const result = await authenticate(unlockKey, verify);
+
+  if (result) {
+    return user.data;
+  }
+
+  throw Error('authentication failed');
 }
