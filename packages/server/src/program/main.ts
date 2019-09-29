a19b5e2c71c439f6e52a26956381bcf4156cbc91
emiwang <2585026122@qq.com>
2019-09-29 15:22:17 +0800

    删除数据库插入测试数据

--- packages/server/src/program/main.ts
+++ packages/server/src/program/main.ts
@@ -1,35 +1,5 @@
-// 断言
-import assert from 'assert';
-
 import bodyParser from 'body-parser';
 import express from 'express';
-import mongodb from 'mongodb';
-
-import {insertTestDocument} from './insertTestData';
-
-const MongoClient = mongodb.MongoClient;
-const url = 'mongodb://localhost:27017';
-const dbName = 'v2pass';
-const client = new MongoClient(url);
-// collect
-client.connect(error => {
-  assert.strictEqual(null, error);
-  console.log('database collect');
-  const db = client.db(dbName);
-  // 注意，每次重启服务器，test document都应当清空重新插入新数据，不让数据积累
-  db.dropCollection('test')
-    .then()
-    .catch();
-  insertTestDocument(db, () => {
-    db.collection('test')
-      .findOne({username: 'emi wang'})
-      .then(data => {
-        const id = String(data._id); // 转化id为客户端所用
-        console.log('id', typeof id, id);
-      })
-      .catch();
-  });
-});
 
 const app = express();
 const jsonParser = bodyParser.json();
