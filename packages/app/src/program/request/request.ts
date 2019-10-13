8b01f4db5af1feb0c43eefb8e10c2b9d060347ee
emiwang <2585026122@qq.com>
2019-10-14 06:00:14 +0800

    请求模块

---
+++ packages/app/src/program/request/request.ts
@@ -0,0 +1,72 @@
+import axios from 'axios';
+
+import UserInfo from '../types/user';
+
+const serverUrl = 'http://localhost:3000/';
+
+export function testUsernameApi(username: UserInfo['username']): Promise<any> {
+  return axios.get(
+    `${serverUrl}testUsernameAvailability?username=${encodeURIComponent(
+      username,
+    )}`,
+  );
+}
+
+export function testEmailApi(email: UserInfo['email']): Promise<any> {
+  return axios.get(
+    `${serverUrl}testEmailAvailability?email=${encodeURIComponent(email)}`,
+  );
+}
+
+export function registerApi(
+  username: UserInfo['username'],
+  email: UserInfo['email'],
+  verify: string,
+): Promise<any> {
+  return axios.post(`${serverUrl}register`, {
+    username,
+    email,
+    verify,
+  });
+}
+
+// 这里data应当是加密后的base64
+export function updateDataApi(
+  id: UserInfo['_id'],
+  data: string,
+  unlockKey: string,
+): Promise<any> {
+  return axios.post(`${serverUrl}updateData`, {
+    id,
+    data,
+    unlockKey,
+  });
+}
+
+export function updateAccountApi(
+  id: UserInfo['_id'],
+  username: UserInfo['username'],
+  email: UserInfo['email'],
+  verify: string,
+  unlockKey: string,
+): Promise<any> {
+  return axios.post(`${serverUrl}updateAccount`, {
+    id,
+    username,
+    email,
+    verify,
+    unlockKey,
+  });
+}
+
+export function getDataApi(
+  username: UserInfo['username'],
+  email: UserInfo['email'],
+  unlockKey: string,
+): Promise<any> {
+  return axios.post(`${serverUrl}getData`, {
+    username,
+    email,
+    unlockKey,
+  });
+}
