1a8c15a957d2b9d2a3bb039414e919afb620e27d
emiwang <2585026122@qq.com>
2019-10-22 03:50:32 +0800

    background页面修改

--- packages/app/src/background/background.ts
+++ packages/app/src/background/background.ts
@@ -1,7 +1,4 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
     chrome.declarativeContent.onPageChanged.addRules([
       {
@@ -9,6 +6,12 @@ chrome.runtime.onInstalled.addListener(() => {
         actions: [new chrome.declarativeContent.ShowPageAction()],
       },
     ]);
+  });
+});
+
+chrome.pageAction.onClicked.addListener(tab => {
+  chrome.storage.sync.get(items => {
+    console.log('chrome storage items', items);
   });
 });
 
