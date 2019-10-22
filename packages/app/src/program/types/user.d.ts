49d218c51b4f80fe4329702db7b01a023316f189
emiwang <2585026122@qq.com>
2019-10-22 10:20:38 +0800

    storage info type

--- packages/app/src/program/types/user.d.ts
+++ packages/app/src/program/types/user.d.ts
@@ -2,9 +2,14 @@ import Target from './target';
 import Password from './password';
 import Vault from './vault';
 import Folder from './folder';
-/**
- * 使用Target接口时，前端和后端都需引入_id
- */
+
+export interface StorageInfo {
+  id: string;
+  username: string;
+  email: string;
+  secretKey: string;
+  data: Buffer;
+}
 
 export interface UserInfo {
   _id: string;
