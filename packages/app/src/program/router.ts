21c9bdead73cd713109b250b6b1addfcc5bc5846
emiwang <2585026122@qq.com>
2019-10-23 02:27:06 +0800

    client router

--- packages/app/src/program/router.ts
+++ packages/app/src/program/router.ts
@@ -1,5 +1,8 @@
+import {message} from 'antd';
 import {Router} from 'boring-router';
 import {createHashHistory} from 'history';
+
+import {getDataApi} from './request';
 
 export const history = createHashHistory();
 
@@ -23,13 +26,28 @@ export const router = Router.create(
 
 export type Router = typeof router;
 
-router.rest.$intercept(() => {
-  chrome.storage.local.get(items => {
-    if (items.id) {
-      router.unlock.$push();
-    } else {
+router.unlock.$afterEnter(() => {
+  // 测试网络，可以离线使用？
+  // 不是每个路由的before enter都需要请求一次数据的吧？
+  getDataApi()
+    .then()
+    .catch(error => {
+      message.error(error.message);
       router.login.$push();
-    }
+    });
+});
+
+router.rest.$intercept(() => {
+  return new Promise(resolve => {
+    chrome.storage.local.get(items => {
+      if (items.id) {
+        router.unlock.$push();
+      } else {
+        router.login.$push();
+      }
+
+      resolve();
+    });
   });
 });
 
