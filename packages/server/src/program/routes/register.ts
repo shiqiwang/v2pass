0944acbde2cbcf7bccc9e65d5d1bb6047a99939b
emiwang <2585026122@qq.com>
2019-10-24 05:28:03 +0800

    注册路由更改

---
+++ packages/server/src/program/routes/register.ts
@@ -0,0 +1,51 @@
+import {RequestHandler} from 'express';
+
+import {
+  ERROR_CODE,
+  SERVER_ERROR,
+  register,
+  registerBaseInfo,
+} from '../dbMethod';
+import {Response} from '../types';
+
+import {testSchema} from './schema';
+
+const resError: Response = {
+  code: ERROR_CODE,
+  data: SERVER_ERROR,
+};
+
+export const registerBaseInfoRoute: RequestHandler = (req, res) => {
+  const paramsTest = testSchema(req.body, ['username', 'email']);
+
+  if (!paramsTest.code) {
+    res.send(paramsTest);
+    return;
+  }
+
+  const {username, email} = req.body;
+
+  registerBaseInfo(username, email)
+    .then(result => res.send(result))
+    .catch(error => {
+      console.error('register baseInfo error', error);
+      res.send(resError);
+    });
+};
+
+export const registerValidatorRoute: RequestHandler = (req, res) => {
+  const paramsTest = testSchema(req.body, ['id', 'verify']);
+
+  if (!paramsTest.code) {
+    res.send(paramsTest);
+    return;
+  }
+
+  const {verify, id} = req.body;
+  register(id, verify)
+    .then(result => res.send(result))
+    .catch(error => {
+      console.error('register validator error', error);
+      res.send(resError);
+    });
+};
