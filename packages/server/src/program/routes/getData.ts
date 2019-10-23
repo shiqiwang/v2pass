189b337b6f927fdb5849caf7361833d083ac2b63
emiwang <2585026122@qq.com>
2019-10-24 05:28:21 +0800

    获取数据路由更改

--- packages/server/src/program/routes/getData.ts
+++ packages/server/src/program/routes/getData.ts
@@ -1,20 +1,29 @@
 import {RequestHandler} from 'express';
 
-import {getData} from '../dbMethod';
-import {authenticateError, generalError} from '../responseMessage';
+import {ERROR_CODE, SERVER_ERROR, getData} from '../dbMethod';
+import {Response} from '../types';
+
+import {testSchema} from './schema';
+
+const resError: Response = {
+  code: ERROR_CODE,
+  data: SERVER_ERROR,
+};
 
 export const getDataRoute: RequestHandler = (req, res) => {
-  const {id} = req.session!;
+  const paramsTest = testSchema(req.body, ['id', 'unlockKey']);
 
-  if (!id) {
-    res.send(authenticateError);
+  if (!paramsTest.code) {
+    res.send(paramsTest);
     return;
   }
 
-  getData(id)
+  const {id, unlockKey} = req.body;
+
+  getData(id, unlockKey)
     .then(result => res.send(result))
     .catch(error => {
       console.error('get data route error', error);
-      res.send(generalError);
+      res.send(resError);
     });
 };
