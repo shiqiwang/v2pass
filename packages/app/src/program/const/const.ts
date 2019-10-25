e669f2538b6e7f5c00129b03f18eb74495c14038
emiwang <2585026122@qq.com>
2019-10-25 09:34:15 +0800

    const文件

---
+++ packages/app/src/program/const/const.ts
@@ -0,0 +1,20 @@
+export const NEW_FOLDER = 'newFolder';
+export const NEW_ITEM = 'newItem';
+export const NEW_RANDOM_PASS = 'newRandomPass';
+export const NEW_VAULT = 'newVault';
+
+export const EDIT = 'edit';
+export const DELETE = 'delete';
+
+export const SUCCESS_CODE = 1;
+export const ERROR_CODE = 0;
+export const SUCCESS = 'ok';
+export const AUTH_FAILED = 'authenticate failed';
+export const USERNAME_EXIST = 'username is occupied';
+export const EMAIL_EXIST = 'email is occupied';
+export const NOT_EXIST = 'not exist';
+export const REGISTRATION_NOT_COMPLETED = 'registration not completed';
+export const SERVER_ERROR = 'internal server error';
+export const PARAMS_VALIDATE_FAILED = 'params type validate failed';
+export const TIME_OUT = 'time out';
+export const REGISTRATION_HAS_COMPLETED = 'registration has completed';
--- packages/app/src/program/const/const.tsx
+++
@@ -1,7 +0,0 @@
-export const NEW_FOLDER = 'newFolder';
-export const NEW_ITEM = 'newItem';
-export const NEW_RANDOM_PASS = 'newRandomPass';
-export const NEW_VAULT = 'newVault';
-
-export const EDIT = 'edit';
-export const DELETE = 'delete';
